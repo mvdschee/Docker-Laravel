43f1de3ff7c2fc66aee2d81df6035523617335b3
Max van der Schee <m.v.d.schee@ewake.nl>
2017-11-21 10:18:13 +0100

    changed script

--- bash.sh
+++ bash.sh
@@ -3,6 +3,7 @@
 if [ "/etc/nginx/conf.d/default.conf" ]; then
   echo "Change default.conf root route"
   sed -i "s|    root /var/www/app;|    root /var/www/app/public;|" /etc/nginx/conf.d/default.conf
+  sed -i "s|         fastcgi_param SCRIPT_FILENAME /var/www/app/index.php;|         fastcgi_param SCRIPT_FILENAME /var/www/app/public/index.php;|" /etc/nginx/conf.d/default.conf
 else
   echo "Error: '/etc/nginx/conf.d/default.conf' not found"
 fi
